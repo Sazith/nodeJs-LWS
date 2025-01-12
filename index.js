412372d19536c53c7531f22711ae38f9d6159cb0
Sazith <sazith.simecsoftdev@gmail.com>
2025-01-12 18:24:25 +0600

    global mnodule

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const _ = require("lodash");
+const _ = require('lodash');
 
-console.log(_.last([1, 2, 5]));
+// console.log(_.last([1, 2, 5]));
+
+// setTimeout(() => {
+//     console.log('test');
+// }, 1000);
+
+// console.log(window);
+
+// console.log(global);
+
+const people = require('./people');
+
+console.log(people);
---
+++ people.js
@@ -0,0 +1,11 @@
+let people = ['shakib', 'tamim', 'mashrafe'];
+
+const a = 6;
+
+function test(){
+  console.log('test');
+  
+}
+console.log(module);
+
+module.exports = people;
